ead4cdff4e722a4d672c8e3f5af2e27181895c8b
rasheedarsalan123 <139682396+rasheedarsalan123@users.noreply.github.com>
2023-11-02 00:56:13 -0700

    Add files via upload

---
+++ class 5/java.js
@@ -0,0 +1,63 @@
+// Q#1//
+// let country=prompt("inetr your Country");
+// let city=prompt("inetr your City");
+// let area=prompt("inetr your Area");
+
+// if (country=="pakistan"){if (city=="karachi"){if (area=="defance"){console.log("welcome")}
+// else {console.log("invalid area")}}else{console.log("invalid area")}}else{console.log("invalid area")};
+
+//Q#2//
+// let user=prompt("What would you like to have tea or coffee?");
+// if(user=="coffe"||user=="coFFe"){console.log("hear is your coffe");}
+// else if(user=="tea"||user=="tEA"){console.log("hear is your tea");}else{console.log("Something went wrong")};
+
+
+//Q#3//
+
+// let user=prompt("They Have A Membership And Student");
+// if(user=="membership"||user=="student"){console.log("you qualify for a discount ");}
+// else{console.log("Sorry, no discount for you")}
+
+//Q#4//
+
+// let user=prompt("Inter YoUR Age");
+// if(user>=18){console.log("yes for valid Id"+"You are eligible");}else{console.log(" No for valid ID"+"Sorry you are not eligible")};
+
+//Q#5//
+// let user=prompt(" your father is paying or sale");
+// if(user=="father paying"||user=="sale"){console.log("You will go shopping");}else{console.log("You will not go shopping")}
+
+//Q#6//
+
+// let user=+prompt("inter your Age");
+// if(user>=17){console.log("you can drive and you own a car");}else{console.log("you can not drive a car")}
+
+//Q#7//
+
+// let user=+prompt("inter number");
+
+// if(user==1){console.log("Monday");}else if(user==2){console.log("Tuesday");}
+// else if(user==3){console.log("Wednesday");}else if(user==4){console.log("Thursday");}
+// else if(user==5){console.log("Friday");}else if(user==6){console.log("Saturday");}
+// else if(user==7){console.log("Sunday");}else{console.log("unreachable number")}
+
+//Q8//
+
+// let user=prompt(" Simulate a traffic light");
+// if(user=="Red"||user=="reD"){console.log(" Stop");}else if(user=="YELLOW"||user=="yellow")
+// {console.log("Slow down");}else if(user=="Green"||user=="GREEN"){console.log("Go");}
+// else{console.log(" Invalid input")}
+
+//Q#9//
+
+// let user=prompt("Inter a day of tha week");
+
+// if(user=="Sunday"){console.log("It.s a Weekend");}else if(user=="Tuesday"){console.log("It’s a Weekday");}
+// else{console.log(" Invalid input")}
+
+//Q10//
+
+// let user = +prompt("Inter your Age");
+
+// if (user>=1&&user <= 12) { console.log("child"); } else if (user <= 18) { console.log("teenager"); } else if (user < 65) { console.log("adult"); } else if (user >65&&user<150) { console.log("above senior"); }
+// else if (user>150){console.log("something went wrong");}else{console.log("invalid age")}
