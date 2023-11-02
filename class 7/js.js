6542c36d189786a22a462fe7c7099f9bf21f4cd5
rasheedarsalan123 <139682396+rasheedarsalan123@users.noreply.github.com>
2023-11-02 01:05:20 -0700

    Add files via upload

---
+++ class 7/js.js
@@ -0,0 +1,49 @@
+// let user=+prompt("inter your number");
+// if(user%2===0){console.log("odd number");}
+// else if(user%1===0){
+//     console.log("even number");
+// }else{console.log("your number is rong")}
+// let obj={
+//     Name:"Arsalan",
+//     FaherName:"Rasheed",
+//     cnic:432875651,
+// };
+// for(let key in obj){
+//     document.write(key+":"+obj[key]+'<br>')
+// }
+
+
+for(let i=1; i<=10; i++){document.write(i +"<br>")};
+
+for(let i=10; i>=1; i--)
+{console.log(i)};
+
+let Array=[ 'apple','banana', 'cherry', 'date' ];
+for(let index in Array){ console.log (Array[index]+index)};
+
+let Table=2
+for(let i=1;i<=10;i++){console.log(Table+"x"+i+"="+Table*i);}
+
+let Table=2
+for(let i=10;i>=1;i--){console.log(Table+"x"+i+"="+Table*i);}
+
+for(let i=1;i<=10;i++){if(i%2==0){console.log(i)};}
+
+for (let i=1;i<=10;i++){if(i%2==1){console.log(i)};}
+
+for (let i=1;i<=5;i++){console.log(i*i)}
+let sum=0
+for(let i =1;i<=5;i++){
+    sum=sum+i
+    
+}
+console.log(sum)
+
+let count = 0
+for(i=1;i<=20;i++){
+    if(i%2==0){
+        count++
+    }
+}
+console.log(count)
+
