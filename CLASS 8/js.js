cf20432099165a025460da46dc885858d2d644b2
rasheedarsalan123 <139682396+rasheedarsalan123@users.noreply.github.com>
2023-11-19 22:58:20 -0800

    Add files via upload

---
+++ CLASS 8/js.js
@@ -0,0 +1,26 @@
+// function Right(userName){
+//     console.log("wellcome"+userName);
+// }
+// Right(" Arsalan");
+// Right(" Sameer");
+// Right(" Zahid");
+
+
+
+// function add(num1,num2){
+// return num1*num2;
+// }let result =add(15,15);
+// console.log(result);
+
+
+let student=[];
+ function person(){
+    let btn ={
+        Name:prompt("inter your name"),
+        FatherName:prompt("inter your FatherName"),
+        ClassName:prompt("inter your ClassName"),
+        
+ } ;
+ student.push(btn);console.log(student)
+  
+} 
