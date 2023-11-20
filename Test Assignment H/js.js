4c901c8f3f8c44c174891b83228c0bc771eb409d
rasheedarsalan123 <139682396+rasheedarsalan123@users.noreply.github.com>
2023-11-19 22:54:34 -0800

    Add files via upload

---
+++ Test Assignment H/js.js
@@ -0,0 +1,84 @@
+Q#4
+let user=+prompt("inter your number");
+if(user<10){console.log(" Less than 10");}
+else if(user>10){console.log("Greater Than 10")}
+
+
+Q#5
+
+let user=+prompt("inter Number");
+
+if(user%2==0){console.log(" even Number");}
+else if(user%1==0){console.log(" odd number");}
+
+Q6
+let user1=+prompt("inter Number");
+let user2=+prompt("inter Number");
+
+console.log(user1+user2)
+
+Q7
+
+let array=["Banana","Cherry",];
+array.push("Apple");
+console.log(array)
+
+Q8
+let array=["Banana","Cherry","Apple"];
+array.pop();
+console.log(array)
+
+Q9
+let array=['apple', 'banana', 'cherry'];
+array.unshift("orange");
+console.log(array)
+
+Q10
+
+let array= ['orange', 'apple', 'banana', 'cherry'];
+array.shift()
+console.log(array)
+
+Q11
+let array=['apple', 'banana', 'cherry'];
+array.splice(1,1);
+console.log(array)
+
+Q12
+
+let array=['orange', 'apple', 'banana', 'cherry'];
+array.reverse();
+console.log(array)
+
+Q13
+let array= ["apple", "banana", "orange", "strawberry"];
+let newarray=array.forEach(array=>{
+console.log(array)
+})
+
+Q14
+let array= ['apple', 'banana', 'cherry'];
+
+array.splice(2,0,"grape");
+console.log(array)
+
+Q15
+
+let Array = [5, 10, 20];
+let abc = 0;
+for (let i = 0; i <Array.length; i++) {
+abc += Array[i];
+}
+console.log(abc);
+
+
+
+
+
+
+
+
+
+
+
+
